e105f7d2bb60b34237bfb738da91d6d67b547610
Baliev Dmitri <bal-dim@yandex.ru>
2018-07-22 11:57:14 +0300

    Added IMutationSettingsProvider interface to provide mutation settings via dependency injection.

---
+++ DawkinsWeasel/Models/Settings/IMutationSettingsProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DawkinsWeasel.Models.Settings
+{
+    /// <summary>
+    /// This interface describes a provider for settings used in the mutation process
+    /// </summary>
+    public interface IMutationSettingsProvider
+    {
+        int Children { get; }
+
+        double Probability { get; }
+
+        string Goal { get; }
+    }
+}
